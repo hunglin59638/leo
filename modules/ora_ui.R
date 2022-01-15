41ed11b2296ca8c4daa5bbf7e2e9c5c63dbf1443
Hung-Lin Chen <hunglin59638@gmail.com>
2022-01-15 02:47:33 +0000

    modify ui

--- modules/ora_ui.R
+++ modules/ora_ui.R
@@ -3,61 +3,55 @@ file_fmt <- c("text/csv",
               "text/comma-separated-values,text/plain",
               ".csv",".tsv", ".xslx")
 
-ora_demo <- function() {}
-
 ora_tabpanel <- 
   tabPanel("Over-Representation Analysis", 
-           fluidRow(
-             column(3, 
-                    sidebarPanel(
-                      width=12, 
-                      h3("Provide gene ids (Entrez Gene)"),
-                      textAreaInput("ora_geneid", NULL, value="",
-                                    width="100%"),
-                      h5("or upload your own file"),
-                      fileInput(inputId="ora_file", label=NULL,
-                                multiple=F, accept=file_fmt),
-                      checkboxInput(inputId="ora_use_demo", 
-                                    label="Use demo data",
-                                    value=FALSE),
-                      selectInput(inputId="species_choice",
-                                  label="Choose species",
-                                  choices=c("Chicken", "Pig", "Human"),
-                                  selected="Chicken"),
-                      selectInput(inputId="db_choice",
-                                  label="Chosse database",
-                                  choices=c("KEGG", "GO"),
-                                  selected="GO"),
-                      conditionalPanel(condition="input.db_choice == 'GO'",
-                                       selectInput("go_ont", "GO ontology",
-                                                   selected="ALL",
-                                                   choices=c("ALL","BP","CC", "MF"))
-                                       
-                      ),
-                      selectInput("padjust",
-                                  label=h4("Adjust p-values for multiple comparisons"),
-                                  choices=c("Bonferroni","Holm","Hochberg", "Hommel",
-                                              "Benjamini & Hochberg","Benjamini & Yekutieli",
-                                              "None"),
-                                  selected="Benjamini & Hochberg"),
-                      numericInput("pvalue_cutoff", 
-                                   label=h4("Cutoff value of pvalue."),
-                                   value=0.05),
-                      numericInput("qvalue_cutoff",
-                                   label=h4("Cutoff value of qvalue."),
-                                   value=0.2),
-                      actionButton("ora_action","Submit")
-                    )
-             ),
-             column(9,
-                    mainPanel(
-                      h2("Over-Representation Analysis"),
-                      DT::dataTableOutput("ora_df"),
-                      uiOutput("ora_f_render"),
-                      hr(),
-                      plotOutput("ora_plot"),
-                      uiOutput("ora_p_render"),
-                    )
+           sidebarLayout(
+             sidebarPanel(
+               width=3,
+               h3("Provide gene ids (Entrez Gene)"),
+               textAreaInput("ora_geneid", NULL, value="", width="100%"),
+               # h5("or upload your own file"),
+               # fileInput(
+               #   inputId="ora_file", label=NULL, multiple=F, accept=file_fmt
+               #   ),
+               selectInput(inputId="ora_species_choice",
+                           label="Choose species",
+                           choices=c("Chicken", "Pig", "Human", "Acipenser ruthenus"),
+                           selected="Chicken"),
+               selectInput(inputId="db_choice",
+                           label="Chosse database",
+                           choices=c("KEGG", "GO"),
+                           selected="GO"),
+               conditionalPanel(condition="input.db_choice == 'GO'",
+                                selectInput("ora_go_ont", "GO ontology",
+                                            selected="ALL",
+                                            choices=c("ALL","BP","CC", "MF"))
+                                ),
+               selectInput("padjust_method",
+                           label=h4("p-values adjust method for multiple comparisons"),
+                           choices=c("Bonferroni", "Holm","Hochberg", "Hommel",
+                                     "Benjamini & Hochberg","Benjamini & Yekutieli",
+                                     "None"),
+                           selected="Benjamini & Hochberg"),
+               numericInput("pvalue_cutoff", 
+                            label=h4("Cutoff value of pvalue."),
+                            value=0.05, max=1, min=0),
+               numericInput("qvalue_cutoff",
+                            label=h4("Cutoff value of qvalue."),
+                            value=0.2, max=1, min=0),
+               fluidRow(
+                 column(2, actionButton("ora_action", "Submit")),
+                 column(2, actionButton("ora_demo", "Example"))
+                 )
+               ),
+             mainPanel(
+               width=9,
+               h2("The Result of Over-Representation Analysis"),
+               DT::dataTableOutput("ora_df"),
+               uiOutput("ora_df_render"),
+               # hr(),
+               # plotOutput("ora_plot"),
+               # uiOutput("ora_p_render"),
+               )
              )
            )
-  )
