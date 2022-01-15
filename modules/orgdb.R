6a669082e9634d8d1782f4484ca867292f35d695
Hung-Lin Chen <hunglin59638@gmail.com>
2022-01-15 02:43:51 +0000

    move ah to interal

--- modules/orgdb.R
+++ modules/orgdb.R
@@ -2,7 +2,18 @@
 if (!require("AnnotationHub")) {
   BiocManager::install("AnnotationHub", quietly = TRUE)}; library(AnnotationHub)
 
-ah <- AnnotationHub()
-org_name <- "Gallus gallus"
-org_info <- subset(ah, species == org_name & rdataclass == 'OrgDb')
-orgdb <- ah[[org_info$ah_id]]
+get_orgdb <- function(organism="Gallus gallus") {
+  ah <- AnnotationHub(ask=FALSE, cache=tools::R_user_dir("AnnotationHub", which="cache"))
+  if (organism %in% c("Chicken", "Gallus gallus")) {
+    organism <- "Gallus gallus"
+  } else if (organism %in% c("Pig", "Sus scrofa")) {
+    organism <- "Sus scrofa"
+  } else if (organism %in% c("Human", "Homo sapiens")) {
+    organism <- "Homo sapiens"
+  } else if (organism %in% c("Sterlet", "Acipenser ruthenus")) {
+    organism <- "Acipenser ruthenus"
+  }
+  org_info <- subset(ah, species == organism & rdataclass == 'OrgDb')
+  return(ah[[org_info$ah_id]])
+}
+
