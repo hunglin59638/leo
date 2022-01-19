b90f788e5c96f29b5e232c2b46d0ae6335156abe
Hung-Lin Chen <hunglin59638@gmail.com>
2022-01-19 22:44:47 +0800

    custom port

--- run.R
+++ run.R
@@ -2,6 +2,12 @@
 library(shiny)
 cmdArgs <- commandArgs(trailingOnly=FALSE)
 root_dir <- normalizePath(dirname((sub("^--file=", "", cmdArgs[grep("^--file=", cmdArgs)]))))[1]
-options(shiny.port = 6523)
+port <- cmdArgs[length(cmdArgs)]
+if (is.na(port) || grepl("^--file=", port, fixed=F)) {
+  port <- 6523
+} else {
+  port <- as.numeric(port)
+}
+options(shiny.port = port)
 options(shiny.host = "0.0.0.0")
 runApp(appDir = root_dir, launch.browser = F)
